283bbefdae68bfe037186b33155052a3bcf0c4dd
MARCO ANTONIO SALAS SALGADO <90123832+MARCPROG27@users.noreply.github.com>
2022-07-28 01:18:53 -0400

    ASCCI JAVA CONVERSION

---
+++ src/main/java/com/mycompany/holamundo/Holamundo.java
@@ -0,0 +1,160 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.holamundo;
+import java.util.Scanner;
+/**
+ *
+ * @author marco
+ */
+public class Holamundo {
+
+    public static void main(String[] args) {
+        // instancias Clase Scanner
+       Scanner entrada = new Scanner(System.in);
+
+// DECLARANDO VARIABLES
+//        String Nombre, Apellido;
+//
+//        System.out.print("Dime tu nombre:");
+//        Nombre = entrada.nextLine();
+//
+//        System.out.print("Dime tu Apellido:");
+//        Apellido = entrada.nextLine();
+// System.out.print("Dime Apellido");
+// Apellido= System.console().readLine
+
+//        System.out.print("HOla" + Nombre + " -" + Apellido);
+
+
+//=============================PROGRAMA 2==========================
+
+//  Scanner entrada = new Scanner(System.in);
+//
+//      int hora;
+//
+//System.out.println("Introduzca una hora (>=0 y <=23:)");
+//hora =entrada.nextInt();
+//
+//
+//if (hora >= 0 && hora < 12) {
+//System.out.println("Muy buenos dias");
+//
+//} else
+//if (hora >= 12 && hora < 21) {
+//System.out.println("Muy buenos tardes");
+//} else
+//if (hora >= 21 && hora < 24) {
+//System.out.println("Muy buenos Noches");
+//} else {
+//System.out.println("Hora no Válida");
+//}
+//System.out.println("Fin de programa");
+// }
+//}
+
+
+    
+    //=====================================SIGUIENTE EJERCICIO
+   // pedir 15 numeros y escribir la suma total con un ciclo
+
+// declarar las variables
+//int num, suma_total;
+//
+//Scanner Entrada = new Scanner(System.in);
+//suma_total = 0;
+//
+//for (int i = 1; i <= 15; i++) {
+//System.out.println("Introduzaca su NÚMERO:"+i);
+//num = Entrada.nextInt();
+//suma_total = suma_total + num;
+//}
+//
+//System.out.print("La suma total es de " + suma_total); 
+//    
+//    
+    
+ // ===========  
+    
+  
+// pedir 15 numeros y escribir la suma total con un ciclo
+
+// declarar las variables
+//int i;
+
+//Scanner Entrada = new Scanner(System.in);
+
+//=========================
+//for (int i = 100; i >= 0; i=i-7) {
+//System.out.println(i);
+//
+//}
+//
+//}
+//
+//}
+//=================
+
+//
+//Scanner Entrada = new Scanner(System.in);
+
+
+//=====================siguiente programa=============
+
+//Scanner Entrada = new Scanner(System.in);
+//int edad, media_edad, suma_edad, mayor_edad, mayor_175;
+//double estatura_media, estatura, suma_estatura;
+//
+//mayor_edad = 0;
+//estatura_media = 0;
+//mayor_175 = 0;
+//suma_edad = 0;
+//suma_estatura = 0;
+//
+//for (int i = 1; i < 5; i++) {
+//System.out.println("Alumno" + i);
+//System.out.println("Introduzca edad:");
+//edad = Entrada.nextInt();
+//System.out.println("Introduzca su estatura:");
+//estatura = Entrada.nextDouble();
+//
+//if (edad > 18) {
+//mayor_edad++;
+//}
+//if (estatura > 1.75) {
+//mayor_175++;
+//}
+//
+//suma_edad = suma_edad + edad;
+//suma_estatura = suma_estatura + estatura;
+//
+//}
+//// promedio de edad
+//media_edad = suma_edad / 5;
+//// promedio de estatura
+//
+//estatura_media = suma_estatura / 5;
+//
+//System.out.println("\n\n El promedio de edades es :" + media_edad);
+//System.out.println("\n\n El promedio de estatura es :" + estatura_media);
+//System.out.println("Mayor de 18 años :" + mayor_edad);
+//System.out.println("Mayores de 1.75 :" + mayor_175);
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//    }
+//}
+
+
+    }
+    
+}
